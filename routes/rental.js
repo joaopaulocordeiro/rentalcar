c268cd4620d57b27229768a94a590dc7de9bdd22
joaopaulocordeiro <34542141+joaopaulocordeiro@users.noreply.github.com>
2019-06-06 17:52:46 -0300

    add delete request in rental

--- routes/rental.js
+++ routes/rental.js
@@ -45,5 +45,11 @@ const {Car} = require('../models/cars')
     res.send(rental);
 })
 
-
 module.exports = router;
+
+
+//DELETE
+    router.post('/', auth, async (res, req)=>{
+        const rental = Rental.findByIdAndRemove(req.body)
+        if(!rental) return res.status(404).send("Rental ID is Not Found")
+    })
